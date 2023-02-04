1a149a4dd3cc78d7cbaca5c52a44f02ab68d284a
ranganikrushank <124433408+ranganikrushank@users.noreply.github.com>
2023-02-04 15:33:33 +0530

    Add files via upload

---
+++ mydetails.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n NAME         :- RANGANI KRUSHANK ");
+    printf("\n ADDRESS      :- BHATAR");
+    printf("\n HOBBIE       :- HACKING  ");
+    printf("\n PHONE NUMBER :- 7778888578");
+    return 0;
+}
