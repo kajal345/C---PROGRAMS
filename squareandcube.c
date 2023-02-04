a7a8d491cad25ede61b260329805ff58a3f45706
ranganikrushank <124433408+ranganikrushank@users.noreply.github.com>
2023-02-04 15:39:11 +0530

    Add files via upload

---
+++ squareandcube.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int no,square,cube;
+    printf("\n Enter the number = ");
+    scanf("%d",&no);
+    square=no*no;
+    cube=no*no*no;
+    printf("\n SQUARE   =  %d",square);
+    printf("\n CUBE     =  %d",cube);
+    return 0;
+}
